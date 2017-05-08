5e5ccdfdfd64f510a82f4ca62ade651adfd6b189
Jaime Pericas <jaime.pericas.saez@gmail.com>
2017-05-07 17:59:38 -0700

    funny styling

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ class Map extends React.Component {
     }, () => {
       const nodes = [], links = [];
       for (let key in this.state.output) {
-        nodes.push({ id: key, label: key, r: 25 });
+        nodes.push({ id: key, label: key, r: 22 });
         this.state.output[key].forEach(parent => {
           links.push({ source: parent, target: key, directed: true });
         });
@@ -53,7 +53,7 @@ class Map extends React.Component {
     }, () => {
       const nodes = [], links = [];
       for (let key in this.state.output) {
-        nodes.push({ id: key, label: key, r: 25 });
+        nodes.push({ id: key, label: key, r: 22 });
         this.state.output[key].forEach(child => {
           links.push({ source: key, target: child, directed: true });
         });
@@ -66,9 +66,9 @@ class Map extends React.Component {
       output: Object.values(book.getChildren(this.state.input_from)),
     }, () => {
       const nodes = [], links = [];
-      nodes.push({ id: this.state.input_from, label: this.state.input_from, r: 25 });
+      nodes.push({ id: this.state.input_from, label: this.state.input_from, r: 22 });
       this.state.output.forEach(nodeName => {
-        nodes.push({ id: nodeName, label: nodeName, r: 25 })
+        nodes.push({ id: nodeName, label: nodeName, r: 22 })
         links.push({ source: this.state.input_from, target: nodeName, directed: true })
       });
       this.graph.redraw(nodes, links);
@@ -79,9 +79,9 @@ class Map extends React.Component {
       output: Object.values(book.getParents(this.state.input_from)),
     }, () => {
       const nodes = [], links = [];
-      nodes.push({ id: this.state.input_from, label: this.state.input_from, r: 25 });
+      nodes.push({ id: this.state.input_from, label: this.state.input_from, r: 22 });
       this.state.output.forEach(nodeName => {
-        nodes.push({ id: nodeName, label: nodeName, r: 25 })
+        nodes.push({ id: nodeName, label: nodeName, r: 22 })
         links.push({ source: nodeName, target: this.state.input_from, directed: true })
       });
       this.graph.redraw(nodes, links)
@@ -92,13 +92,13 @@ class Map extends React.Component {
       output: book.getAdjacent(this.state.input_from),
     }, () => {
       const nodes = [], links = [];
-      nodes.push({ id: this.state.input_from, label: this.state.input_from, r: 25 });
+      nodes.push({ id: this.state.input_from, label: this.state.input_from, r: 22 });
       this.state.output.children.forEach(nodeName => {
-        nodes.push({ id: nodeName, label: nodeName, r: 25 })
+        nodes.push({ id: nodeName, label: nodeName, r: 22 })
         links.push({ source: this.state.input_from, target: nodeName, directed: true })
       });
       this.state.output.parents.forEach(nodeName => {
-        nodes.push({ id: nodeName, label: nodeName, r: 25 })
+        nodes.push({ id: nodeName, label: nodeName, r: 22 })
         links.push({ source: nodeName, target: this.state.input_from, directed: true })
       });
       this.graph.redraw(nodes, links);
