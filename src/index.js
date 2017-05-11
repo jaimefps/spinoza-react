634b30746da119e68daa69846c0784a4243f6877
Jaime Pericas <jaime.pericas.saez@gmail.com>
2017-05-11 10:33:01 -0700

    add greuler to pack.json and minor corrections

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Map from './components/app';
+import App from './components/app';
 
 ReactDOM.render(
-  <Map />,
+  <App />,
   document.getElementById('root'),
 );
