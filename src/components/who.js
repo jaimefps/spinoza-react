1d67f76fbe8f2bb4a2ccb56a125214a126595052
Jaime Pericas Saez <jaimefps@Jaimes-MacBook-Pro.local>
2017-12-22 20:52:41 -0800

    typo fix

--- src/components/who.js
+++ src/components/who.js
@@ -9,12 +9,12 @@ export default class Who extends React.Component {
         <div className="contact-links">
           <h1 className="author">Jaime Pericás</h1>
           <p>
-            Software engineer with an academic background in Math, Philosphy, and Art History.
-            Since writing his first line of code, Jaime has been enthralled by code's creative power.
+            Software engineer with an academic background in Philosphy, and Art History, and Math.
+            Since writing his first lines of code, Jaime has been enthralled by code's creative power.
             <br/><br/>
             You can contact him at: jaime.pericas.saez@gmail.com
             <br/><br/>
-            Or send an invite on any of the following plataforms.
+            Or send an invite on any of the following platforms.
           </p>
           <a className="link linkedin" target="_blank" href="https://www.linkedin.com/in/jaime-pericas-saez/" />
           <a className="link github" target="_blank" href="https://github.com/jaimefps" />
