9b3cfba56233f4bd080dacc36cdbee7a5af9cdf9
Jaime Pericas Saez <jaimefps@Jaimes-MBP.domain>
2018-01-27 11:00:25 -0800

    cleanup Graph class

--- src/lib/graph.js
+++ src/lib/graph.js
@@ -1,75 +1,86 @@
 export class Node {
-  constructor(title, content) {
+  constructor(title) {
     this.title = title;
-    this.content = content;
     this.parents = [];
     this.children = [];
   }
 }
+
 export default class Graph {
   constructor() {
     this.nodes = {};
   }
-  addNode(node, content) {
-    this.nodes[node] = new Node(node, content);
-    return this.nodes[node];
+  addNode(node) {
+    this.nodes[node] = new Node(node);
   }
   addEdge(fromNode, toNode) {
     this.nodes[fromNode].children.push(toNode);
     this.nodes[toNode].parents.push(fromNode);
   }
-  parents(name) {
-    const results = [];
-    this.nodes[name].parents.forEach(name => results.push(name));
-    return results;
+  parents (node) {
+    return this.getParents(node);
   }
-  children(name) {
-    const results = [];
-    this.nodes[name].children.forEach(name => results.push(name));
-    return results;
+  getParents(node) {
+    return this.nodes[node].parents;
   }
-  getAdjacent(name) {
-    const result = {
-      children: this.children(name),
-      parents: this.parents(name),
+  children(node) {
+    return this.getChildren(node);
+  }
+  getChildren(node) {
+    return this.nodes[node].children;
+  }
+  getAdjacent(node) {
+    return {
+      children: this.getChildren(node),
+      parents: this.getParents(node),
+    };
+  }
+  getAncestors(node) {
+    const graph = this;
+    const ancestors = {};
+    function subroutine (node) {
+      ancestors[node] = graph.getParents(node);
+      if (graph.getParents(node).length < 1) {
+        return;
+      }
+      graph.getParents(node).forEach((parent) => {
+        subroutine(parent);
+      });
     }
-    return result;
+    subroutine(node);
+    return ancestors;
   }
-  connection(from, to) {
+  getDescendants(node) {
+    const graph = this;
+    const descendants = {};
+    function subroutine (node) {
+      descendants[node] = graph.getChildren(node);
+      if (graph.getChildren(node).length < 1) {
+        return;
+      }
+      graph.getChildren(node).forEach((child) => {
+        subroutine(child);
+      });
+    }
+    subroutine(node);
+    return descendants;
+  }
+  connection (from, to) {
+    return this.getConnection(from, to);
+  }
+  getConnection(from, to) {
     const connectors = {};
-    const descendantsEtAl = this.getDescendantsGreuler(from);
-    const ancestorsEtAl = this.getAncestorsGreuler(to);
-    for (let key in descendantsEtAl) {
-      if (ancestorsEtAl[key]) connectors[key] = descendantsEtAl[key];
+    const descendants = this.getDescendants(from);
+    const ancestors = this.getAncestors(to);
+    for (let key in descendants) {
+      if (ancestors[key]) {
+        connectors[key] = descendants[key];
+      }
     }
     for (let key in connectors) {
-      connectors[key] = connectors[key].filter(el => connectors[el]);
+      connectors[key] = connectors[key].filter((el) => connectors[el]);
     }
     return connectors;
-  }
-  getAncestorsGreuler(title) {
-    const propsWithParents = {};
-    const graph = this;
-    (function getParents(nodeName) {
-      propsWithParents[nodeName] = graph.nodes[nodeName].parents;
-      if (graph.nodes[nodeName].parents.length < 1) return;
-      graph.nodes[nodeName].parents.forEach((parent) => {
-        getParents(parent);
-      }) 
-    }(title))
-    return propsWithParents;
-  }
-  getDescendantsGreuler(title) {
-    const propsWithChildren = {}
-    const graph = this;
-    (function getChildren(nodeName) {
-      propsWithChildren[nodeName] = graph.nodes[nodeName].children;
-      if (graph.nodes[nodeName].children.length < 1) return;
-      graph.nodes[nodeName].children.forEach((child) => {
-        getChildren(child);
-      })
-    }(title))
-    return propsWithChildren;
   }
 }
 
